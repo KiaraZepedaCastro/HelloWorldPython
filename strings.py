93edc78f5fec54d1cb142ffc0608476928a1e9dc
KiaraZepedaCastro <zepedakd@gmail.com>
2023-01-13 16:06:41 -0700

    :)

---
+++ strings.py
@@ -0,0 +1,23 @@
+course = "Python for Beginners"
+print(course)
+course = '''
+Hello reader,
+
+This is a long string.
+
+Thank you,
+Writer'''
+
+print(course)
+
+course = "Python for Beginners"
+print(course[0])
+print(course[0:3])
+print(course[0:])
+print(course[:5])
+print(course[:])
+another = course[:]
+print(another)
+
+name = "Jennifer"
+print(name[1:-1])
