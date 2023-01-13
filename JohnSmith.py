f5a3adba57226d2e0ca13b377f01e3be8a0d4d07
KiaraZepedaCastro <zepedakd@gmail.com>
2023-01-12 20:57:58 -0700

    :)

---
+++ JohnSmith.py
@@ -0,0 +1,5 @@
+name = "John Smith"
+status = "new patient"
+is_new = True
+age = 20
+print(name)
--- app.py
+++ app.py
@@ -2,3 +2,10 @@ print("Kiara Zepeda Castro")
 print('o----')
 print(' ||||')
 print('*' * 10)
+price = 10
+price = 20
+rating = 4.9
+name = 'Mosh'
+is_published = False
+print(price)
+print(name)
