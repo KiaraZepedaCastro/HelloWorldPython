a3f582952aa58124063bd0a2f1e9fc314defb621
KiaraZepedaCastro <zepedakd@gmail.com>
2023-01-15 18:09:43 -0700

    :)

---
+++ arithmeticOperations.py
@@ -0,0 +1,17 @@
+print(10 + 3 - 4)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+x = 10
+x = x + 3
+print(x)
+
+y = 10
+y += 3
+print(y)
+
+a = 10
+a -= 3
+print(a)
---
+++ stringMethods.py
@@ -0,0 +1,9 @@
+course = "Python for Beginners"
+print(len(course))
+print(course.upper())
+print(course)
+print(course.lower())
+print(course.find("P"))
+print(course.replace("Python", "Coding"))
+print("Python" in course)
+course.title()
