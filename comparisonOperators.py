6c126374db44fc26a4f1dadd72c6d4eb2a8e12d3
KiaraZepedaCastro <zepedakd@gmail.com>
2023-01-17 20:59:44 -0700

    :)

---
+++ comparisonOperators.py
@@ -0,0 +1,14 @@
+temperature = 30
+
+if temperature >= 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+name = "Juan Garcia"
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name can be a maximum of 50 characters")
+else:
+    print("Name looks good!")
