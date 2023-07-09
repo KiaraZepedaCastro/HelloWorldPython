2fe231328b1b30fe574e529cb517df5edb7d4b47
KiaraZepedaCastro <zepedakd@gmail.com>
2023-07-08 17:43:37 -0700

    :)

---
+++ emojiConverter.py
@@ -0,0 +1,32 @@
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)": "😀",
+    "(:": "😀",
+    ":(": "☹️",
+    "):": "☹️",
+    ":/": "😶",
+    "/:": "😶"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+    print(output)
+
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "😀",
+        "(:": "😀",
+        ":(": "☹️",
+        "):": "☹️",
+        ":/": "😶",
+        "/:": "😶"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+message = input(">")
+print(emoji_converter(message))
---
+++ functions.py
@@ -0,0 +1,16 @@
+def greet_user(first_name, last_name):
+    print(f'Hello {first_name} {last_name}!')
+    print('Welcome aboard')
+
+
+print("Start")
+greet_user("John", "Smith")
+print("Finish")
+
+def square(number):
+    return number * number
+
+result = square(3)
+print(result)
+
+print(square(3))
